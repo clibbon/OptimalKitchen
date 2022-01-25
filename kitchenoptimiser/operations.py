4bdab9621f7add7664a822b99fff37661a9e210d
ClibbonA <alex.clibbon@kantar.com>
2022-01-25 00:05:48 +0000

    EOD commit

--- kitchenoptimiser/operations.py
+++ kitchenoptimiser/operations.py
@@ -1,99 +1,96 @@
+import abc
 from collections import defaultdict
 from functools import reduce
 import random
 import numpy as np
+from abc import ABC, abstractmethod
 
 
-def mutate_complex(gene, swap_chance, fixed_positions):
-    """Randomly shuffles places of a few things in the gene"""
+class Operator(ABC):
+    """Parent class defining operations"""
 
-    locations_to_update = []
-    assigned_things = reduce(lambda x, y: x.union(y), gene.values(), set())
-    unassigned_things = ALL_THINGS.difference(assigned_things).difference(fixed_positions.values())
+    @abstractmethod
+    def create_gene(self):
+        """Creates a gene in the form of a dictionary of thing:location"""
+        pass
 
-    for location, things in gene.items():
+    @abstractmethod
+    def mutate(self, individual) -> None:
+        """In place mutation of an individual"""
+        pass
 
-        if location in fixed_positions.keys():
-            continue
-
-        if np.random.rand() < swap_chance:
-            locations_to_update.append(location)
-            gene[location] = set()
-            unassigned_things.update(things)
-
-    # Need to introduce some randomness
-    random.shuffle(locations_to_update)
-
-    for location in locations_to_update:
-        available_space = 1.  # Starts off empty
-
-        while available_space >= SMALLEST_THING:
-            available_sizes = size_possiblethings_dict.keys()
-            largest_size = max(size for size in available_sizes if size <= available_space)
-
-            candidate_things = unassigned_things.intersection(size_possiblethings_dict[largest_size])
-
-            if len(candidate_things) == 0:
-                break  # No candidate things left
-
-            selected_thing = random.choice(list(candidate_things))
-            gene[location].add(selected_thing)
-            unassigned_things.remove(selected_thing)
-            available_space -= selected_thing.size
-
-    del gene.fitness.values
-    return gene,
+    @abstractmethod
+    def mate(self, ind_1, ind_2) -> tuple[dict, dict]:
+        pass
 
 
-def get_child_complex(parent_1, parent_2):
-    """Creates a child by selecting positions from either parent. In the case
-    no valid positions (from either parent) are free then randomly assign a
-    position"""
-    unassigned_things = ALL_THINGS.difference(FIXED_POSITIONS.values())
-    empty_locations = set()
-    child = defaultdict(set)
+class SetOperator(Operator):
 
-    non_fixed_locations = ALL_LOCATIONS.difference(FIXED_POSITIONS.keys())
+    def __init__(self, all_things, all_locations, fixed_assignments, swap_chance):
+        self.all_things = all_things
+        self.all_locations = all_locations
+        self.fixed_assignments = fixed_assignments
+        self.smallest_thing = get_smallest_thing(all_things)
 
-    for location in non_fixed_locations:
-        if np.random.rand() > .5:
-            # Try parent 1
-            parent_1_things = parent_1[location]
-            if parent_1_things.issubset(unassigned_things):
-                child[location] = parent_1_things
-                unassigned_things.difference_update(parent_1_things)
-            else:
-                empty_locations.add(location)
+        self.assignable_things = set(all_things.keys()).difference(set(fixed_assignments.keys()))
+        self.assignable_locations = set(all_locations.keys()).difference(set(fixed_assignments.values()))
 
-        else:
-            # Parent 2
-            parent_2_things = parent_2[location]
-            if parent_2_things.issubset(unassigned_things):
-                child[location] = parent_2_things
-                unassigned_things.difference_update(parent_2_things)
-            else:
-                empty_locations.add(location)
+        self.swap_chance = swap_chance
 
-    unassigned_locations = list(empty_locations)
-    random.shuffle(unassigned_locations)
+    def create_gene(self):
+        empty_locations = list(self.assignable_locations)
+        random.shuffle(empty_locations)
 
-    for location in unassigned_locations:
-        available_space = 1.  # Starts off empty
+        gene = dict(self.fixed_assignments)
+        self.assign_things(gene, empty_locations, set(self.assignable_things))
 
-        while available_space >= SMALLEST_THING:
-            available_sizes = size_possiblethings_dict.keys()
-            largest_size = max(size for size in available_sizes if size <= available_space)
+        return gene
 
-            candidate_things = unassigned_things.intersection(size_possiblethings_dict[largest_size])
+    def mutate(self, individual) -> tuple:
+        locations_to_update = []
+        unassigned_things = self.assignable_things.difference(set(individual.keys()))
 
-            if len(candidate_things) == 0:
-                child[location] = set()
-                break  # No candidate things left
+        for location, things in individual.items():
 
-            selected_thing = random.choice(list(candidate_things))
-            child[location].add(selected_thing)
-            available_space -= selected_thing.size
+            if location not in self.assignable_locations:
+                continue
 
-    child.update(FIXED_POSITIONS)
-    assert child.keys() == parent_1.keys(), f"PARENT 1\n{parent_1}\n\nPARENT 2\n{parent_2}\n\nCHILD\n{child}\n\nDIFFERENCE\n{set(child.keys()).symmetric_difference(set(parent_1.keys()))}"
-    return child
+            if np.random.rand() < self.swap_chance:
+                locations_to_update.append(location)
+                individual[location] = set()
+                unassigned_things.update(things)
+
+        random.shuffle(locations_to_update)  # Need to introduce some randomness
+
+        self.assign_things(individual, locations_to_update, unassigned_things)
+
+        del individual.fitness.values  # part of the process to update individuals inplace
+        return individual,
+
+    def mate(self, ind_1, ind_2) -> tuple[dict, dict]:
+
+
+
+    def assign_things(self, gene: dict, locations, things) -> None:
+        for location in locations:
+            available_space = 1.  # Starts off empty
+
+            while available_space >= self.smallest_thing:
+                available_sizes = size_possiblethings_dict.keys()
+                largest_size = max(size for size in available_sizes if size <= available_space)
+
+                candidate_things = things.intersection(size_possiblethings_dict[largest_size])
+
+                if len(candidate_things) == 0:
+                    if location not in gene:
+                        gene[location] = set()
+                    break  # No candidate things left
+
+                selected_thing = random.choice(list(candidate_things))
+                gene[location].add(selected_thing)
+                available_space -= selected_thing.size
+                things.remove(selected_thing)
+
+
+def get_smallest_thing(things) -> float:
+    pass
